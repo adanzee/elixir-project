cb1938ec5f4de8518370b9b9ac313bc7502778ce
adanzee <adan.khurram00@gmail.com>
2026-06-26 10:57:53 +0500

    genserver

---
+++ lib/vidsync/meetings/roomServer.ex
@@ -0,0 +1,22 @@
+defmodule Vidsync.Meetings.RoomServer do
+  use GenServer, restart: :temporary
+  alias Vidsync.Meetings.Room
+
+  @impl true
+  def init(room) do
+    {:ok, room}
+  end
+
+  defp via_tuple(room_id) do
+    {:via, Registry, {Vidsync.RoomRegistry, room_id}}
+  end
+
+  def start_link(room = %Room{}) do
+    GenServer.start_link(__MODULE__, room , [name: via_tuple(room.id)])
+  end
+
+  @impl true
+  def handle_call(:get_state, _from, current_room_state) do
+    {:reply, current_room_state, current_room_state}
+  end
+end
